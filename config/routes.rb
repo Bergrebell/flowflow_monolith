21e0fb4170bd602411d46be3c3a621df8e477803
Roman Kuepper <rrkuepper@gmail.com>
2021-12-20 18:30:03 +0100

    adds gitignore and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    resources :stations, only: %i[index show]
+    get 'station(/:id)/weather', to: 'stations#weather', as: :station_weather
+    get 'station(/:id)/history', to: 'stations#history', as: :station_history
+
+    get 'measurements', to: 'measurements#index'
+
+    get :weather, to: 'weather#at_location'
+  end
+
+  # See CHECKS-file in root. This performs a simple check by dokku to see if the
+  # server started after a deployment
+  get '/check.txt', to: proc {[200, {}, ['it_works']]}
 end
